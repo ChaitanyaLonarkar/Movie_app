f2901fa69e96ee886f9f4a4262344835c9c7a13b
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-02-24 21:50:27 +0530

    added files

--- src/MovieBox.jsx
+++ src/MovieBox.jsx
@@ -13,21 +13,6 @@ export default function MovieBox(props) {
         </div>
         <h6>{data.title}</h6>
         <button className="detail-btn btn btn-success p-1 mt-1  mb-3" key={data.id} onClick={()=>{show(true);id(data.id)}}>View More</button>
-
-        {/* <div className="details">
-          <p>
-            <b>Release date : </b>
-            {data.release_date}
-          </p>
-          <p>
-            <b>Description : </b>
-            {data.overview}
-          </p>
-          <p>
-            <b>Original language : </b>
-            {data.original_language}
-          </p>
-        </div> */}
       </div>
     </>
   );
